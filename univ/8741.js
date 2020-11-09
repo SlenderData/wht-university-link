1c1c9eeaf8d153fbcfe09b936b863c68801ce693
SlenderData <74196745+SlenderData@users.noreply.github.com>
2020-11-09 23:37:54 +0800

    Create 8741.js

---
+++ univ/8741.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "太湖创意职业技术学院",
+    id: "8741",
+    link: [ {
+        name: "创意主页",
+        url: "http://www.thcyzy.org/html/index.asp"
+    }, {
+        name: "太湖官网",
+        url: "http://www.wxu.edu.cn/"
+    }, {
+        name: "智慧团建",
+        url: "https://zhtj.youth.cn/zhtj/"
+    }, {
+        name: "职教云",
+        url: "https://zjy2.icve.com.cn/portal/login.html"
+    }, {
+        name: "江苏省教育考试院",
+        url: "https://www.jseea.cn/"
+    }, {
+        name: "MOOC",
+        url: "https://www.icourse163.org/"
+    }, {
+        name: "网易公开课",
+        url: "https://open.163.com/"
+    } ]
+};
